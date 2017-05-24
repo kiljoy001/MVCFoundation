72b9b46d30c97ca62c079e448ddd34b9d619b176
Scott Guyton <kiljoy001@gmail.com>
2017-05-23 22:19:14 -0700

    finished exercise 4-1

---
+++ MVCFoundation/Controllers/BooksController.cs
@@ -0,0 +1,97 @@
+﻿using MVCFoundation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFoundation.Controllers
+{
+    public class BooksController : Controller
+    {
+        static List<Book> books = new List<Book>
+        {
+            new Book{Id=1, ISBN="123456789", Title="Fantasy"},
+            new Book{Id=2, ISBN="234567890", Title="Science Fiction"},
+            new Book{Id=3, Title="<script>alert('You have been hacked!');</script>", ISBN="345678901"}
+        };
+        // GET: Books
+        public ActionResult Index()
+        {
+            var model = books.OrderByDescending(b => b.Title);
+            return View(model);
+        }
+
+        // GET: Books/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Books/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Books/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Books/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Books/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Books/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
